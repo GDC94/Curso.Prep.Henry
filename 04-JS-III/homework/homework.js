14a83ae5cbdfd364978bb0ea712af3654c53f323
GDC94 <germanderbescatoni@gmail.com>
2020-09-23 23:58:35 +0200

    Finish Homework 8

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -98,7 +98,7 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-  var maximo = numeros[0];
+  var NumeroM aximo = numeros[0];
   for(var i = 1; i < numeros.length; i++) {
     if(numeros[i] > maximo) { 
       maximo = numeros[i];
